59530bce0c7f1cbcd5863b4ba578ca0cabef5525
Shlomi Baruch <shlomibaruch5@gmail.com>
2021-08-16 23:22:39 +0300

    gh-

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Switch>
         <Route
           exact
-          path="/"
+          path="/books"
           render={(props) => (<SearchBook {...props} />)}
         />
         <Route
