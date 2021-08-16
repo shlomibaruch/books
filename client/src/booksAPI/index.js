049a74176652f62284c45def22304d282d811080
Shlomi Baruch <shlomibaruch5@gmail.com>
2021-08-16 23:26:39 +0300

    handle https req

--- client/src/booksAPI/index.js
+++ client/src/booksAPI/index.js
@@ -1,9 +1,9 @@
 const getApiSearchUrl = (searchTerm) =>
-  `http://openlibrary.org/search.json?q=${searchTerm}`;
+  `https://openlibrary.org/search.json?q=${searchTerm}`;
 
 export const getBookCoverByOLID = (olid) =>
 olid ?
-  `http://covers.openlibrary.org/b/olid/${olid}-M.jpg` : 
+  `https://covers.openlibrary.org/b/olid/${olid}-M.jpg` : 
   "https://cdn.pixabay.com/photo/2016/10/14/16/39/book-1740515_960_720.png";
 
 export const searchBooks = (searchTerm = "") => {
